5ac18dbf8a6b7a608ab55a69f6cdd0ebd78ee61c
hayapo <keep.forward1001@gmail.com>
2021-02-22 01:01:08 +0900

    :sparkles:lsdの追加

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,16 @@
 set PATH $HOME/.cargo/env $PATH
-bass source $HOME/.cargo/env
 set PATH /usr/local/bin/ecs-cli $PATH
+
+if test ! -e ~/.dircolors/dircolors.ansi-dark
+    git clone https://github.com/seebi/dircolors-solarized.git ~/.dircolors
+end
+eval (dircolors -c ~/.dircolors/dircolors.ansi-dark)
+
+function cd
+    builtin cd $argv
+        lsd --depth 1
+end
+
+alias lsd "lsd --tree -I "target" -I "node_modules" -I "fonts" -I "Downloads" "
+alias ll "lsd --depth 2"
+alias ls "lsd --depth 1"
