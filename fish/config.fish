831dc794a8ca40899c0e1e213f99d55c6e888163
hayapo <keep.forward1001@gmail.com>
2021-02-25 23:34:37 +0900

    :sparkles:mypyの導入

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 alias open "explorer.exe"
 set PATH $HOME/.cargo/env $PATH
 set PATH /usr/local/bin/ecs-cli $PATH
+set PATH $HOME/.local/bin/ $PATH
 set PATH $HOME/.cargo/bin $PATH
 set PATH $HOME/.pyenv/bin $PATH
 if test ! -e ~/.dircolors/dircolors.ansi-dark
