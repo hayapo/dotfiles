6aca213fd71c2df6fe75fd6a780bf10483070676
hayapo <keep.forward1001@gmail.com>
2022-09-20 14:52:51 +0900

    :art: ちょっとへんこう

--- cmd/fish.sh
+++ cmd/fish.sh
@@ -3,6 +3,6 @@
 # fish init
 if has "fisher" ; then
     curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
-
+fi
 # plugin install
 copy -f ~/dotfiles/.config/fish/fish_plugins ~/dotfiles/.config/fish/fishfile
