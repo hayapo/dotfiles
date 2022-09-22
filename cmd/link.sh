04c3ff8a0d290776ca570ff770a75e8bf4cac90c
hayapo <keep.forward1001@gmail.com>
2022-09-23 00:10:35 +0900

    :art: aa

--- cmd/link.sh
+++ cmd/link.sh
@@ -1,7 +1,6 @@
 # ./bin.sh
 ln -sf ~/dotfiles/.config/fish ~/.config/fish
 ln -sf ~/dotfiles/.config/starship.toml ~/.config/starship.toml
-ln -sf ~/dotfiles/.config/gh ~/.config/gh
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.hushlogin ~/.hushlogin
 ln -sf ~/dotfiles/.profile ~/.profile
