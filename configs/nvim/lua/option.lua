17007e5d606eced20790fe6b9a3125f4bf10f6b0
hayapo <keep.forward1001@gmail.com>
2022-11-24 16:35:49 +0900

    :zap: Tab widthの変更

--- configs/nvim/lua/option.lua
+++ configs/nvim/lua/option.lua
@@ -1,10 +1,10 @@
 vim.cmd('autocmd!')
 vim.opt.number = true
 vim.opt.autoindent = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.smartindent = true
-vim.opt.showtabline = 2
+vim.opt.showtabline = 4
 vim.opt.breakindent = true
 vim.opt.swapfile = false
 vim.opt.title = true
