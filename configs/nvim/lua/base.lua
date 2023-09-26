9992d0b50d741495d2f641815629f639d451930a
hayapo <keep.forward1001@gmail.com>
2023-09-26 13:00:24 +0900

    added neovim

---
+++ configs/nvim/lua/base.lua
@@ -0,0 +1,13 @@
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+vim.opt.number = true
+vim.wo.relativenumber = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.showtabline = 4	
+vim.opt.clipboard = "unnamedplus"
+vim.opt.helplang = "ja"
---
+++ configs/nvim/lua/plugins.lua
@@ -0,0 +1,45 @@
+return {
+	  -- the colorscheme should be available when starting Neovim
+	{
+		"folke/tokyonight.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			-- load the colorscheme here
+			vim.cmd([[colorscheme tokyonight]])
+		end,
+	},
+	{
+		"folke/which-key.nvim",
+		lazy = true
+	},
+	{
+		"lambdalisue/fern.vim"
+	},
+	{
+		"lambdalisue/fern-git-status.vim"
+	},
+	{
+		"lambdalisue/glyph-palette.vim"
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = true,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("nvim-tree").setup {}
+		end
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons"
+		},
+		config = function()
+			require("plugins/101-lualine")
+		end
+	},
+}
