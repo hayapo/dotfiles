e37e72e13dcb4c35cdc18a08b602e433658d5ef3
hayapo <keep.forward1001@gmail.com>
2023-10-01 23:32:47 +0900

    fix-typo: Fix the typo (neovim -> nvim)

--- cmd/link.sh
+++ cmd/link.sh
@@ -10,6 +10,6 @@ ln -sf ${dotfiles_dir}/.zshrc $HOME/.zshrc
 
 
 ln -sf ${dotfiles_dir}/.config/starship.toml $HOME/.config/starship.toml
-ln -sf ${dotfiles_dir}/.config/neovim/ $HOME/.config/
-ln -sf ${dotfiles_dir}/.config/fish $HOME/.config/
-ln -sf ${dotfiles_dir}/.config/sheldon $HOME/.config/
+ln -sf ${dotfiles_dir}/.config/nvim/ $HOME/.config/nvim
+ln -sf ${dotfiles_dir}/.config/fish $HOME/.config/fish
+ln -sf ${dotfiles_dir}/.config/sheldon $HOME/.config/sheldon
